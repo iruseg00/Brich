66c9cf24c3dca58f8a2b8a32261a94b29d3a21a5
iruseg00 <isiruseg@gmail.com>
2021-11-24 23:21:32 +0300

    main implemented(3/4 CRUD)

--- server/controllers/PostsController.mjs
+++ server/controllers/PostsController.mjs
@@ -1,14 +1,7 @@
 import express from "express";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
 import { Blob } from "node:buffer";
 
 import PostService from "../services/PostService.mjs";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const filePath = path.join(__dirname, "posts.json");
 const router = express.Router();
 
 router.get("/all", async (req, res) => {
