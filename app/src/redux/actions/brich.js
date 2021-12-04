f802e9d54b4713835a3f8e0baa8efabb4b6d716c
iruseg00 <isiruseg@gmail.com>
2021-12-04 13:40:23 +0300

    main app

--- app/src/redux/actions/brich.js
+++ app/src/redux/actions/brich.js
@@ -1,81 +1,87 @@
 import {
-	REQUEST,
-	SUCCESS_REQUEST,
-	FAILED_REQUEST,
-	ABOUT_REQUEST,
-	GET_POSTS,
-	DOWNLOAD_JSON,
-	UPLOAD_JSON,
-} from '../actionsTypes/brich';
+  REQUEST,
+  SUCCESS_REQUEST,
+  FAILED_REQUEST,
+  ABOUT_REQUEST,
+  GET_POSTS,
+  DOWNLOAD_JSON,
+  UPLOAD_JSON,
+} from "../actionsTypes/brich";
 import {
-	getInfoAboutMe,
-	sendPost as serviceSendPost,
-	getAllPosts as getAllPostsService,
-	downloadJSON as downloadJSON_service,
-	uploadJSON as uploadJSONService,
-} from '../../services/brich';
-import { message } from 'antd';
+  getInfoAboutMe,
+  sendPost as serviceSendPost,
+  getAllPosts as getAllPostsService,
+  downloadJSON as downloadJSON_service,
+  uploadJSON as uploadJSONService,
+} from "../../services/brich";
+import { message } from "antd";
 
 export const getProfileInfo = () => {
-	return async (dispatch) => {
-		try {
-			dispatch({ type: REQUEST });
-			const data = await getInfoAboutMe();
-			dispatch({ type: ABOUT_REQUEST, payload: data });
-		} catch {
-			dispatch({ type: FAILED_REQUEST });
-		}
-	};
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST });
+      const data = await getInfoAboutMe();
+      dispatch({ type: ABOUT_REQUEST, payload: data });
+    } catch {
+      dispatch({ type: FAILED_REQUEST });
+    }
+  };
 };
 
 export const sendPost = (data) => {
-	return async (dispatch) => {
-		try {
-			dispatch({ type: REQUEST });
-			const info = await serviceSendPost(data);
-			dispatch({ type: SUCCESS_REQUEST, payload: info });
-			message.success('Успешно!');
-		} catch {
-			message.error('Ошибка');
-		}
-	};
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST });
+      const info = await serviceSendPost(data);
+      dispatch({ type: SUCCESS_REQUEST, payload: info });
+      message.success("Успешно!");
+    } catch {
+      message.error("Ошибка");
+    }
+  };
 };
 
 export const getAllPosts = () => {
-	return async (dispatch) => {
-		try {
-			dispatch({ type: REQUEST });
-			const posts = await getAllPostsService();
-			dispatch({ type: GET_POSTS, payload: posts });
-			message.success('Успешно!');
-		} catch {
-			message.error('Ошибка');
-		}
-	};
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST });
+      const posts = await getAllPostsService();
+      dispatch({ type: GET_POSTS, payload: posts });
+      message.success("Успешно!");
+    } catch {
+      message.error("Ошибка");
+    }
+  };
 };
 
 export const downloadJSON = () => {
-	return async (dispatch) => {
-		try {
-			dispatch({ type: REQUEST });
-			const file = await downloadJSON_service();
-			dispatch({ type: DOWNLOAD_JSON, payload: file });
-			message.success('Успешно!');
-		} catch {
-			message.error('Ошибка');
-		}
-	};
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST });
+      const file = await downloadJSON_service();
+      dispatch({ type: DOWNLOAD_JSON, payload: file });
+      message.success("Успешно!");
+    } catch {
+      message.error("Ошибка");
+    }
+  };
 };
 
 export const uploadJSON = (file) => {
-	return async (dispatch) => {
-		try {
-			dispatch({ type: REQUEST });
-			const data = await uploadJSONService(file);
-			dispatch({ type: UPLOAD_JSON, payload: data });
-			message.success('Успешно!');
-		} catch {
-			message.error('Ошибка');
-		}
-	};
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST });
+      const formData = new FormData();
+      formData.append("file", file);
+      console.log(formData);
+      const data = await uploadJSONService(formData, {
+        "Content-Type": "multipart/form-data",
+      });
+      dispatch({ type: UPLOAD_JSON, payload: data });
+      message.success("Успешно!");
+    } catch (error) {
+      console.log(error);
+      message.error("Ошибка");
+    }
+  };
 };
--- app/src/views/brichProfile/BrichProfile.js
+++ app/src/views/brichProfile/BrichProfile.js
@@ -1,96 +1,104 @@
-import FileSaver from 'file-saver';
-import { Button, Form, Input, Spin, Upload } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
-import { LoadingOutlined } from '@ant-design/icons';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import FileSaver from "file-saver";
+import { Button, Form, Input, Spin, Upload } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import { LoadingOutlined } from "@ant-design/icons";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
-	getProfileInfo,
-	sendPost as sendPostAction,
-	getAllPosts,
-	downloadJSON,
-	uploadJSON,
-} from '../../redux/actions/brich';
-import style from './style.module.scss';
-import FormItem from 'antd/lib/form/FormItem';
+  getProfileInfo,
+  sendPost as sendPostAction,
+  getAllPosts,
+  downloadJSON,
+  uploadJSON,
+} from "../../redux/actions/brich";
+import style from "./style.module.scss";
+import FormItem from "antd/lib/form/FormItem";
 
 const BrichProfile = () => {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(getProfileInfo());
-	}, [dispatch]);
-	const state = useSelector((store) => store.brich.about);
-	const posts = useSelector((store) => store.brich.posts);
-	const loading = useSelector((store) => store.brich.loading);
-	const file = useSelector((store) => store.brich.file);
-	const antIcon = <LoadingOutlined style={{ fontSize: 64 }} spin />;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProfileInfo());
+  }, [dispatch]);
+  const state = useSelector((store) => store.brich.about);
+  const posts = useSelector((store) => store.brich.posts);
+  const loading = useSelector((store) => store.brich.loading);
+  const file = useSelector((store) => store.brich.file);
+  const antIcon = <LoadingOutlined style={{ fontSize: 64 }} spin />;
 
-	const sendPost = (sendData) => {
-		const objectToSend = {
-			title: sendData.titleInput,
-			text: sendData.descriptionInput,
-		};
-		dispatch(sendPostAction(objectToSend));
-	};
+  const sendPost = (sendData) => {
+    const objectToSend = {
+      title: sendData.titleInput,
+      text: sendData.descriptionInput,
+    };
+    dispatch(sendPostAction(objectToSend));
+  };
 
-	const renderPosts = (arr) => {
-		return arr.map((el) => {
-			return (
-				<div key={el.postId}>
-					<div>Заголовок: {el.title}</div>
-					<div>Текст: {el.text}</div>
-				</div>
-			);
-		});
-	};
+  const renderPosts = (arr) => {
+    return arr.map((el) => {
+      return (
+        <div key={el.postId}>
+          <div>Заголовок: {el.title}</div>
+          <div>Текст: {el.text}</div>
+        </div>
+      );
+    });
+  };
 
-	async function getData(dataa) {
-		console.log(dataa);
-		let obj = await fetch(dataa);
-		console.log(obj);
-		dispatch(uploadJSON(obj));
-	}
+  async function getData() {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.addEventListener("change", function (event) {
+      const File = event.target.files[0];
+      console.log(File);
+      File && dispatch(uploadJSON(File));
+    });
+    input.click();
+  }
 
-	if (file.data) {
-		console.log(file);
-		const blob = new Blob([JSON.stringify(file.data)], { type: 'application/json' });
-		FileSaver.saveAs(blob, 'posts.json');
-	}
+  // if (file.data) {
+  //   console.log(file);
+  //   const blob = new Blob([JSON.stringify(file.data)], {
+  //     type: "application/json",
+  //   });
+  //   FileSaver.saveAs(blob, "posts.json");
+  // }
 
-	return loading ? (
-		<div className={style.spinContainer}>
-			<Spin indicator={antIcon} size='large' />
-		</div>
-	) : (
-		<div>
-			<div>Surname: {state?.data?.surname}</div>
-			<div>Middlename: {state?.data?.middleName}</div>
-			<div>Email: {state?.data?.email}</div>
-			<div>USERID: {state?.data?.userID}</div>
-			<Form onFinish={sendPost} name='postForm'>
-				<FormItem name='titleInput'>
-					<Input placeholder='Введите заголовок поста' />
-				</FormItem>
-				<FormItem name='descriptionInput'>
-					<Input placeholder='Введите текст поста' />
-				</FormItem>
-				<Button htmlType='submit' type='primary'>
-					Отправить
-				</Button>
-				<Button onClick={() => dispatch(getAllPosts())} type='default'>
-					Получить посты
-				</Button>
-				<Button onClick={() => dispatch(downloadJSON())} type='default'>
-					Скачать посты (JSON)
-				</Button>
-			</Form>
-			<Upload showUploadList={false} onChange={getData}>
-				<Button icon={<UploadOutlined />}>Click to Upload</Button>
-			</Upload>
-			{console.log(posts)}
-			{posts.data ? renderPosts(posts.data) : null}
-		</div>
-	);
+  return loading ? (
+    <div className={style.spinContainer}>
+      <Spin indicator={antIcon} size="large" />
+    </div>
+  ) : (
+    <div>
+      <div>Surname: {state?.data?.surname}</div>
+      <div>Middlename: {state?.data?.middleName}</div>
+      <div>Email: {state?.data?.email}</div>
+      <div>USERID: {state?.data?.userID}</div>
+      <Form onFinish={sendPost} name="postForm">
+        <FormItem name="titleInput">
+          <Input placeholder="Введите заголовок поста" />
+        </FormItem>
+        <FormItem name="descriptionInput">
+          <Input placeholder="Введите текст поста" />
+        </FormItem>
+        <Button htmlType="submit" type="primary">
+          Отправить
+        </Button>
+        <Button onClick={() => dispatch(getAllPosts())} type="default">
+          Получить посты
+        </Button>
+        <Button onClick={() => dispatch(downloadJSON())} type="default">
+          Скачать посты (JSON)
+        </Button>
+      </Form>
+      <Upload showUploadList={false}>
+        <Button onClick={getData} icon={<UploadOutlined />}>
+          Click to Upload
+        </Button>
+      </Upload>
+      {console.log(posts)}
+      {posts.data ? renderPosts(posts.data) : null}
+    </div>
+  );
 };
 
 export default BrichProfile;
